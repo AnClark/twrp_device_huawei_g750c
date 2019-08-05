0be682378900f633ea4cb0b4ac379fd1bbb18b67
AnClark Liu <anclarkliu@outlook.com>
2019-08-05 20:10:21 +0800

    BoardConfig: SEPolicy: Use Qualcomm legacy policy config
    Old Qualcomm SoCs (e.g. MSM8974, MSM8628) use legacy SEPolicy configs.
    Some former component's rules are defined there, for example,
    mpdecision, the Qualcomm hotplug service.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -120,7 +120,7 @@ TARGET_RIL_VARIANT := caf
 TARGET_QCOM_NO_FM_FIRMWARE := true
 
 # SELinux
--include device/qcom/sepolicy/sepolicy.mk
+include device/qcom/sepolicy-legacy/sepolicy.mk
 BOARD_SEPOLICY_DIRS += device/huawei/g750c/sepolicy
 
 # Vold
